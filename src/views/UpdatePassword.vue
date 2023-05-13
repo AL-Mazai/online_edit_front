4aabf883c746a43dbec57adfde2b2570b8eeee55
QSUNTOY <132589973+QSUNTOY@users.noreply.github.com>
2023-05-13 15:43:23 +0800

    个人信息和修改密码界面

--- src/views/UpdatePassword.vue
+++ src/views/UpdatePassword.vue
@@ -1,14 +1,115 @@
 <!--更改密码-->
 <template>
-
+    <div class="update-wrap">
+        <el-form class="update-container">
+            <h1 class="title">修改密码</h1>
+            <el-form-item>
+                <el-input
+                    type="text"
+                    placeholder="原密码"
+                    v-model="user.password"
+                    autocomplete="off"
+                ></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-input
+                    type="text"
+                    placeholder="新密码"
+                    v-model="user.newPassword"
+                    autocomplete="off"
+                ></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-input
+                    type="text"
+                    placeholder="确认密码"
+                    v-model="user.checkPassword"
+                    autocomplete="off"
+                ></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="doUpdate" style="width: 100%">
+                    确认更改
+                </el-button>
+            </el-form-item>
+        </el-form>
+    </div>
 </template>
 
 <script>
 export default {
-    name: "UpdatePassword"
+    name: "UpdatePassword",
+    data() {
+        return {
+            user: {
+                password: "",
+                newPassword: "",
+                checkPassword: ""
+            },
+            rules: {//表单规则
+                password: [
+                    { required: true, message: '请输入原密码', trigger: 'blur' },
+                    { min: 6, max: 20, message: '长度在 6 到 20 个字符', trigger: 'blur' }
+                ],
+                newPassword: [
+                    { required: true, message: '请输入新密码', trigger: 'blur' },
+                    { min: 6, max: 20, message: '长度在 6 到 20 个字符', trigger: 'blur' }
+                ],
+                checkPassword: [
+                    { required: true, message: '请再次输入新密码', trigger: 'blur' },
+                    { min: 6, max: 20, message: '长度在 6 到 20 个字符', trigger: 'blur' }
+                ],
+            },
+            dialogFormVisible: false,
+
+        };
+    },
+    methods: {
+        doUpdate() {
+            //发送请求
+            this.axios.post("http://localhost:8088/user/updatePassword", this.user).then((res) => {
+                //打印收到的数据（用于测试）
+                console.log(res.data)
+                //验证
+                if (res.status === 200) {
+                    this.$message.success("修改成功")
+                    this.$router.push('/')
+                }
+            }).catch((err) =>  {//异常
+                if(err.response.status === 401){
+                    this.$message.error(err.response.data)
+                }
+            });
+        },
+    }
 }
 </script>
 
 <style scoped>
+.update-wrap {
+    box-sizing: border-box;
+    width: 100%;
+    height: 100%;
+    padding-top: 10%;
+    background-repeat: no-repeat;
+    background-position: center right;
+    background-size: 100%;
+}
 
+.update-container {
+    border-radius: 10px;
+    margin: 0px auto;
+    width: 350px;
+    padding: 30px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    text-align: left;
+    box-shadow: 0 0 20px 2px rgba(0, 0, 0, 0.1);
+}
+
+.title {
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #505458;
+}
 </style>
--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -1,14 +1,87 @@
 <!--个人信息-->
 <template>
+    <div class="avatar" style="text-align: center">
+        <div>
+            <el-avatar :size="100"  src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+        </div>
+        <el-descriptions class="Info" title="   " :column="1"  border>
+            <el-descriptions-item>
+                <template slot="label">
+                    <i class="el-icon-user" ></i>
+                    用户ID
+                </template>
+                {{user.userId}}
+            </el-descriptions-item>
+            <el-descriptions-item>
+                <template slot="label">
+                    <i class="el-icon-monitor"></i>
+                    用户名
+                </template>
+                {{user.userName}}
+                <el-link icon="el-icon-edit" style="color: cornflowerblue; left: 5%"></el-link>
+            </el-descriptions-item>
+            <el-descriptions-item>
+                <template slot="label">
+                    <i class="el-icon-message"></i>
+                    邮箱
+                </template>
+                {{user.email}}
+            </el-descriptions-item>
+            <el-descriptions-item>
+                <template slot="label">
+                    <i class="el-icon-switch-button"></i>
+                    会员
+                </template>
+                <el-tag size="small">是</el-tag>
+            </el-descriptions-item>
+            <el-descriptions-item>
+                <template slot="label">
+                    <i class="el-icon-cloudy"></i>
+                    云空间
+                </template>
+                <el-link icon="el-icon-refresh" style="color: cornflowerblue" >同步</el-link>
+            </el-descriptions-item>
+        </el-descriptions>
+    </div>
 
 </template>
 
+
 <script>
+
 export default {
-    name: "UserInfo"
+
+    // eslint-disable-next-line vue/multi-word-component-names
+
+    data(){
+        return{
+            user: {
+                userId: "",
+                userName: "",
+                email: "",
+            },
+        }
+    },
+    methods:{
+        reset(){
+            this.userName = '';
+            this.userId='';
+            this.email=''
+        },
+        // async submit(){
+        //     var name = sessionStorage.getItem("userName");
+        //     const {data:res} = await this.$http.post(`select/${name}`)
+        //     this.userList=res.data
+        // },
+    }
 }
+
 </script>
 
-<style scoped>
-
+<style>
+.Info{
+    width: 45%;
+    text-align: center;
+    margin: auto;
+}
 </style>
